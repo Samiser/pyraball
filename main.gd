a260458790b09df662dddc8e0a27669109417e50
Samiser <github@me.samiser.xyz>
2025-09-03 18:35:53 +0000

    added size change on swapping world

--- main.gd
+++ main.gd
@@ -3,7 +3,7 @@ extends Node3D
 @onready var player: Player = $Player
 
 enum Level {BACK, PRESENT, FORWARD}
-var current_level: Level = Level.BACK
+var current_level: Level = Level.PRESENT
 
 func get_level_rotation(level: Level) -> Vector3:
 	match level:
@@ -23,12 +23,20 @@ func _on_rotation(direction: String, player_position: Vector3) -> void:
 		current_level = posmod(current_level - 1, 3)
 	elif direction == "right":
 		current_level = posmod(current_level + 1, 3)
-	print(current_level)
+
+	match current_level:
+		Level.BACK:
+			player.set_new_scale(0.2)
+		Level.PRESENT:
+			player.set_new_scale(0.5)
+		Level.FORWARD:
+			player.set_new_scale(3)
+
 	var tween := create_tween()
 	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
 	tween.tween_property($PyraWorld, "rotation", get_level_rotation(current_level), 0.3)
 	await tween.finished
-	player.on_rotation_completed(player_position)
+	player.rotation_completed(player_position)
 
 func _ready() -> void:
 	$Player.rotate.connect(_on_rotation)
