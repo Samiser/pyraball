0db4a871b9fcd94745d0d4a89dd4d624840cbf45
Samiser <github@me.samiser.xyz>
2025-09-04 11:12:59 +0000

    added world transforms on rotate and refactored world

--- main.gd
+++ main.gd
@@ -1,42 +1,4 @@
 extends Node3D
 
-@onready var player: Player = $Player
-
-enum Level {BACK, PRESENT, FORWARD}
-var current_level: Level = Level.PRESENT
-
-func get_level_rotation(level: Level) -> Vector3:
-	match level:
-		Level.BACK:
-			return Vector3(deg_to_rad(65), 0, 0)
-		Level.PRESENT:
-			return Vector3(0, deg_to_rad(90), deg_to_rad(65))
-		Level.FORWARD:
-			return Vector3(-deg_to_rad(65), deg_to_rad(180), 0)
-		#Level.VOID:
-			#return Vector3(0, deg_to_rad(270), -deg_to_rad(65))
-	
-	return Vector3.ZERO
-
-func _on_rotation(direction: String, player_position: Vector3) -> void:
-	if direction == "left":
-		current_level = posmod(current_level - 1, 3)
-	elif direction == "right":
-		current_level = posmod(current_level + 1, 3)
-
-	match current_level:
-		Level.BACK:
-			player.set_new_scale(0.2)
-		Level.PRESENT:
-			player.set_new_scale(0.5)
-		Level.FORWARD:
-			player.set_new_scale(3)
-
-	var tween := create_tween()
-	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
-	tween.tween_property($PyraWorld, "rotation", get_level_rotation(current_level), 0.3)
-	await tween.finished
-	player.rotation_completed(player_position)
-
 func _ready() -> void:
-	$Player.rotate.connect(_on_rotation)
+	$Player.rotate.connect($World._on_rotation)
---
+++ world.gd
@@ -0,0 +1,61 @@
+extends Node3D
+
+@onready var environment: Environment = $WorldEnvironment.environment
+@export var player: Player
+@export var minimap_camera: Camera3D
+
+enum Level {BACK, PRESENT, FORWARD}
+var current_level: Level = Level.PRESENT
+
+func get_level_rotation(level: Level) -> Vector3:
+	match level:
+		Level.BACK:
+			return Vector3(deg_to_rad(65), 0, 0)
+		Level.PRESENT:
+			return Vector3(0, deg_to_rad(90), deg_to_rad(65))
+		Level.FORWARD:
+			return Vector3(-deg_to_rad(65), deg_to_rad(180), 0)
+		#Level.VOID:
+			#return Vector3(0, deg_to_rad(270), -deg_to_rad(65))
+	
+	return Vector3.ZERO
+
+func _tween_fog_color(new_color: Color, new_density: float) -> void:
+	var tween := create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(environment, "fog_light_color", new_color, 1)
+	tween.parallel().tween_property(environment, "fog_density", new_density, 1)
+	tween.parallel().tween_property(minimap_camera, "environment:fog_light_color", new_color, 1)
+
+func _tween_rotation(new_rotation: Vector3) -> void:
+	var tween := create_tween()
+	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
+	tween.tween_property($PyraWorld, "rotation", get_level_rotation(current_level), 0.3)
+	await tween.finished
+	return
+
+func _apply_world_changes() -> void:
+	match current_level:
+		Level.BACK:
+			player.set_new_scale(0.2)
+			_tween_fog_color(Color.from_hsv(0.6, 0.6, 1.0), 0.15)
+		Level.PRESENT:
+			player.set_new_scale(0.5)
+			_tween_fog_color(Color.from_hsv(0.7, 0.6, 1.0), 0.05)
+		Level.FORWARD:
+			player.set_new_scale(3)
+			_tween_fog_color(Color.from_hsv(0.95, 0.6, 1.0), 0.01)
+
+func _change_current_level(direction: String) -> void:
+	if direction == "left":
+		current_level = posmod(current_level - 1, 3)
+	elif direction == "right":
+		current_level = posmod(current_level + 1, 3)
+
+func _on_rotation(direction: String, player_position: Vector3) -> void:
+	_change_current_level(direction)
+	_apply_world_changes()
+	await _tween_rotation(get_level_rotation(current_level))
+	player.rotation_completed(player_position)
+
+func _ready() -> void:
+	_apply_world_changes()
