3d3444fa87448656a41582ee5c2c18b14ab187a4
wookbee <159189510+wookbee@users.noreply.github.com>
2025-09-08 11:08:11 +0100

    added respawn failsafe

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -45,10 +45,17 @@ var last_safe_pos : Vector3
 var is_respawning := false
 var respawn_camera := false
 @onready var hand_mesh := $respawn_hand
-
+var backup_respawn_points :Array[Vector3]= [
+Vector3(-80, 44,-120),
+Vector3(80, 44,-120),
+Vector3(-50, 44,-45),
+Vector3(50, 44,-45),
+Vector3(0, 44,55)
+]
 var was_grounded := false
 var has_jumped := false
 var coyote_timer := 0.0
+var respawn_time := 0.0
 
 func _ready() -> void:
 	$MapMarker.visible = true
@@ -247,6 +254,14 @@ func respawn_player() -> void:
 	last_safe_pos += (last_safe_pos - fall_pos).normalized() * 2.0
 	last_safe_pos.y = safe_height
 	
+	if (Time.get_ticks_msec() - respawn_time) < 5000:
+		var closest_dist := 99999
+		for pos in backup_respawn_points:
+			var dist := global_position.distance_to(pos)
+			if dist < closest_dist:
+				last_safe_pos = pos
+				closest_dist = dist
+	
 	respawn_sfx.play()
 	
 	# hand comes down
@@ -290,4 +305,5 @@ func respawn_player() -> void:
 	
 	hand_mesh.visible = false
 	freeze = false
+	respawn_time = Time.get_ticks_msec()
 	is_respawning = false
