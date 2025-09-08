75efe60c41140c8e8d9e213320cb6f86db16ce7d
Samiser <github@me.samiser.xyz>
2025-09-08 17:53:18 +0000

    animated cage for simon says, working on gameplay

--- Scripts/simon_says.gd
+++ Scripts/simon_says.gd
@@ -1,10 +1,60 @@
 extends Node3D
 
 @onready var buttons: Array[TriangleButton] = [$Green, $Blue, $Red, $Yellow]
+@export var cage: Cage
+
+var game_sequence: Array[TriangleButton]
+var player_inputting: bool = false
+var player_sequence_step: int = 0
+
+func _play_game_animation() -> void:
+	while not player_inputting:
+		for button in game_sequence:
+			if player_inputting:
+				return
+			await cage.pulse(_button_to_color(button))
+			await get_tree().create_timer(1).timeout
+		await get_tree().create_timer(4).timeout
+
+func _add_random_button_to_sequence() -> void:
+	game_sequence.append(buttons.pick_random())
+
+func _start_game() -> void:
+	_add_random_button_to_sequence()
+	_play_game_animation()
+
+func _button_to_color(button: TriangleButton) -> Color:
+	match button.name:
+		"Green": return Color.GREEN
+		"Blue": return Color.BLUE
+		"Red": return Color.RED
+		"Yellow": return Color.YELLOW
+	
+	printerr("unknown color: ", name)
+	return Color.WHITE
 
 func _on_button_pressed(button: TriangleButton) -> void:
-	print(button)
+	player_inputting = true
+	cage.pulse(_button_to_color(button))
+	if player_sequence_step < game_sequence.size() and button == game_sequence[player_sequence_step]:
+		print("correct!")
+		if game_sequence[-1] == button:
+			print("final step!")
+			player_inputting == false
+			_add_random_button_to_sequence()
+			print(game_sequence)
+			_play_game_animation()
+	else:
+		print("incorrect")
+		game_sequence = []
+		player_sequence_step = 0
+		for i in range(3):
+			await cage.pulse(_button_to_color(button))
+			await get_tree().create_timer(0.2).timeout
+		_add_random_button_to_sequence()
+		_play_game_animation()
 
 func _ready() -> void:
 	for button: TriangleButton in buttons:
 		button.pressed.connect(_on_button_pressed)
+	_start_game()
---
+++ cage.gd
@@ -0,0 +1,30 @@
+extends Node3D
+class_name Cage
+
+@onready var bars := $Bars.get_children()
+
+func _ready() -> void:
+	print(bars)
+
+func open() -> void:
+	print(bars)
+	var tween := create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	tween.set_parallel()
+	for i in bars.size():
+		tween.tween_property(bars[i], "rotation:x", deg_to_rad(-150), 4).set_delay(i * 0.15)
+
+func reset() -> void:
+	for bar in bars:
+		bar.rotation.x = deg_to_rad(-28)
+
+func pulse(color: Color) -> void:
+	var material: Material = $Ball/MeshInstance3D.get_active_material(0)
+	var tween := create_tween()
+
+	material.emission = color
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(material, "emission_energy_multiplier", 1, 0.7)
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(material, "emission_energy_multiplier", 0, 0.7)
+	await tween.finished
