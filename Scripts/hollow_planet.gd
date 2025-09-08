7905b435cf2eefe51c65f2ae548b581dea995351
wookbee <159189510+wookbee@users.noreply.github.com>
2025-09-08 15:05:21 +0100

    made planet spin, increased player jump force in big ball mode, made clouds less annoying

--- Scripts/hollow_planet.gd
+++ Scripts/hollow_planet.gd
@@ -1,9 +1,12 @@
 extends Node3D
 
 var has_player_entered := false
+@onready var sphere := $CSGSphere3D
+
+func _process(delta: float) -> void:
+	sphere.rotate_y(0.1 * delta)
 
 # removed old functionality - might not need this script anymore
-
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if !body is Player:
 		return
