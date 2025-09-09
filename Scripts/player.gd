122a9496e99ee6192b9002c1a2b969e59288eb9e
Samiser <github@me.samiser.xyz>
2025-09-09 20:09:49 +0000

    prevented switching time while frozen

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -157,9 +157,9 @@ func _input(event: InputEvent) -> void:
 		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 			get_viewport().set_input_as_handled()
-	elif event.is_action_pressed("rotate_left") and not all_crystals_are_collected and past_unlocked:
+	elif event.is_action_pressed("rotate_left") and not all_crystals_are_collected and past_unlocked and not freeze:
 		_rotate("left")
-	elif event.is_action_pressed("rotate_right") and not all_crystals_are_collected and past_unlocked:
+	elif event.is_action_pressed("rotate_right") and not all_crystals_are_collected and past_unlocked and not freeze:
 		_rotate("right")
 	elif event.is_action_pressed("unlock_all"):
 		past_unlocked = true
