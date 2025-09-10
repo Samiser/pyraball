bcc57f4fbbd8f26230e1298a55af16cfb400c21e
wookbee <159189510+wookbee@users.noreply.github.com>
2025-09-10 01:55:04 +0100

    added animation to main menu

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -4,8 +4,15 @@ extends Control
 @export var options_button: Button
 @export var exit_button: Button
 
+var time := 0.0
+
 signal play
 
 func _ready() -> void:
 	play_button.pressed.connect(func() -> void: play.emit())
 	exit_button.pressed.connect(func() -> void: get_tree().quit())
+
+func _process(delta: float) -> void:
+	time += delta
+	$VBoxContainer/CenterContainer/logo_text/logo_bg.rotation += delta * 0.1
+	$VBoxContainer/CenterContainer/logo_text.self_modulate = Color.WHITE * (0.8 + sin(time * 2.0) * 0.2)
